ba77300c6b04a3d24dd0e8d4a0c236969c267561
Jean-Cairoli <jcairoli1993@gmail.com>
2022-09-07 11:41:35 -0600

    added about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="about">
-    <h1>Hello {{ their_name }}, this is an about page</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+    <h1>Hello I'm Jean Cairoli,</h1>
+    <p>I'm a Full Stack Developer and enthusiast, I love the idea of creating innovative Solutions where needed, I'm looking for a position where I can apply this passion for development, I'm eager to find a place where I'm able to exercise my creativity and my problem-solving abilities,
+I am a curious person, and I'm always looking for ways to help, furthermore I'm ready to learn whatever is needed</p>
   </div>
 </template>
 
